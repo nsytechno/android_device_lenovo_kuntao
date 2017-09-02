9228d7434bd8dcfb476a4434097e587cd765deba
FireLord1 <firelord.xda@gmail.com>
2017-09-02 22:06:58 +0530

    kuntao: Dont build audio hw, we're using stock for now
    * Add missing audio lib to build after rebase
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- device.mk
+++ device.mk
@@ -64,9 +64,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
-    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
