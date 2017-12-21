476482a1a73584ff42474e00d924a9d864d2fa38
nsytechno <nsytechno@gmail.com>
2017-12-21 13:45:09 +0530

    DNM: Build a dummy android.hidl.base@1.0 for Oreo blobs

--- device.mk
+++ device.mk
@@ -305,3 +305,7 @@ PRODUCT_COPY_FILES += \
     kernel/lenovo/msm8953/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0
